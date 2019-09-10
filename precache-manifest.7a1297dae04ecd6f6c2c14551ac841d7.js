42458e08898187d4c5cb9a66575ce0949c735fc0
Patryk Keska <patrykkeska4@gmail.com>
2019-09-10 11:56:40 +0200

    Updates

---
+++ precache-manifest.7a1297dae04ecd6f6c2c14551ac841d7.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d622bc7d22c9b127f60e573cd87831b",
+    "url": "/weather_forecast/index.html"
+  },
+  {
+    "revision": "d40fd6568793a63c5565",
+    "url": "/weather_forecast/static/js/2.251ca7f6.chunk.js"
+  },
+  {
+    "revision": "75aa96d6592264f28891",
+    "url": "/weather_forecast/static/js/main.2f38b138.chunk.js"
+  },
+  {
+    "revision": "21e67b570c824474ecaa",
+    "url": "/weather_forecast/static/js/runtime~main.d02c3fd9.js"
+  },
+  {
+    "revision": "09745bd14b32325b2e918c64718ecb68",
+    "url": "/weather_forecast/static/media/Drizzle.09745bd1.jpg"
+  },
+  {
+    "revision": "44ccc5eaaec35bb28b36af3c30e01b01",
+    "url": "/weather_forecast/static/media/brokenClouds.44ccc5ea.jpg"
+  },
+  {
+    "revision": "79fe21672dbd397f99ee7bbfa2817ba4",
+    "url": "/weather_forecast/static/media/clearSky.79fe2167.jpg"
+  },
+  {
+    "revision": "458e22316522f95037e1063d32594a14",
+    "url": "/weather_forecast/static/media/errorImg.458e2231.jpg"
+  },
+  {
+    "revision": "5578cf1683a5db7ff83d975820d5f1cc",
+    "url": "/weather_forecast/static/media/fewClouds.5578cf16.jpg"
+  },
+  {
+    "revision": "b27a4e9e064c9e634ddaed7c6e621dd2",
+    "url": "/weather_forecast/static/media/fog.b27a4e9e.jpg"
+  },
+  {
+    "revision": "de43a9c8deb373734073b232010b32c9",
+    "url": "/weather_forecast/static/media/heavyRain.de43a9c8.jpg"
+  },
+  {
+    "revision": "0670e21f5076552656459d14990b240f",
+    "url": "/weather_forecast/static/media/heavySnow.0670e21f.jpg"
+  },
+  {
+    "revision": "e57ce67148bb7f8a89952fc6a84e431e",
+    "url": "/weather_forecast/static/media/heavyStorm.e57ce671.jpg"
+  },
+  {
+    "revision": "50902c64c3e2c0f50458bb256183e257",
+    "url": "/weather_forecast/static/media/lightRain.50902c64.jpg"
+  },
+  {
+    "revision": "df1c625e84c8abed9c293c6414f56ab1",
+    "url": "/weather_forecast/static/media/lightSnow.df1c625e.jpg"
+  },
+  {
+    "revision": "2fb6fbd9eca6d3db5fb226766923b540",
+    "url": "/weather_forecast/static/media/mainBackground.2fb6fbd9.jpg"
+  },
+  {
+    "revision": "17675593e4222ed1269646e314c46f37",
+    "url": "/weather_forecast/static/media/sleet.17675593.jpg"
+  },
+  {
+    "revision": "995547a2fb200e3c171f3a1eeeb49d60",
+    "url": "/weather_forecast/static/media/squalls.995547a2.jpg"
+  },
+  {
+    "revision": "051898ef5bbbc67491ca9eabe1c9fe3b",
+    "url": "/weather_forecast/static/media/tornado.051898ef.jpg"
+  }
+]);
